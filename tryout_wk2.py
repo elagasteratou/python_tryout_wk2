105c1dbde55a31db3f3c3e0da247e8407bfa6217
jackie <jackie.woodburn@gmail.com>
2021-02-01 18:15:58 +0000

    fixed bug in JW edits that allowed for all integers

--- tryout_wk2.py
+++ tryout_wk2.py
@@ -1,7 +1,6 @@
 
 good_luck_msg = ["Today is your lucky day", "Be brave! you got this", "Take it nice and easy"]
 
-
 choice = int(input("Choose a number between 1-3"))
 if choice == 1:
     print(good_luck_msg[0])
@@ -25,15 +24,19 @@ elif choice > 2 or choice < 1:
 #         elif choice == 3:
 #             print(good_luck_msg[2])
 #             break
+#         else:
+#             print("enter a number between 1 and", len(good_luck_msg))
 #     except:
 #         print("enter a number between 1 and", len(good_luck_msg))
 
-### JW EDIT 2 ###
+## JW EDIT 2 ###
 # while True:
 #     try:
 #         choice = int(input("Choose a number between 1-3 "))
-#         if choice == 1 or 2 or 3:
+#         if choice in {1,2,3}:
 #             break
+#         else:
+#             print("enter a number between 1 and", len(good_luck_msg))
 #     except:
 #         print("enter a number between 1 and", len(good_luck_msg))
 #
