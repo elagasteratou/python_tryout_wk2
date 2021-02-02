9cf6797a64a746ff83fad8cb0df47fa83787ce5f
jackie <jackie.woodburn@gmail.com>
2021-02-02 15:04:32 +0000

    Edited function file so that advice won't repeat

--- tryout_functions.py
+++ tryout_functions.py
@@ -5,18 +5,29 @@ import random
 
 def give_advice(dictionary, adjectives):
     user_cont = 1
+    used = []
+    issue_gen = (random.randint(1, len(dictionary))) - 1
     while user_cont == 1:
-        issue_gen = (random.randint(1, len(dictionary))) - 1
+        while issue_gen in used:
+            issue_gen = (random.randint(1, len(dictionary))) - 1
         q = str("Do you feel " + adjectives[issue_gen] + "? ")
         choice_1 = yes_no_q(q)
+        used.append(issue_gen)
 
         if choice_1 == "y":
             print(dictionary[adjectives[issue_gen]])
-            q = "Do you want more advice? "
-            choice_2 = yes_no_q(q)
-            if choice_2 == "n":
-                user_cont =0
+            if len(adjectives) == len(used):
+                print("... and that was the last piece of advice we had! Sorry :( ")
+                user_cont = 0
+            else:
+                q = "Do you want more advice? "
+                choice_2 = yes_no_q(q)
+                if choice_2 == "n":
+                    user_cont =0
         else:
+            if len(adjectives) == len(used):
+                print("Sorry, we're all out of advice :( ")
+                user_cont = 0
             pass
 
 # Function asks Y/N question and moderates user input.
